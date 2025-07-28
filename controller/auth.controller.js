acc3c641f16ddba6a9c7f80bfdffb683377fa35e
calevv <ika818488@gmail.com>
2025-07-28 17:12:37 +0900

    feat:백엔드 토큰 ing

---
+++ controller/auth.controller.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
+
+const authController = {};
+
+const secretKey = process.env.JWT_SECRET_KEY;
+
+authController.authenticate = async (req, res) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) {
+      throw new Error("invalid token");
+    }
+    const token = tokenString.replace("Bearer", "");
+    jwt.verify(token, secretKey, (error, payload) => {
+      if (error) {
+        throw new Error("invalid token");
+      }
+      // res.status(200).json({ status: 'ok', userId: payload._id });
+    });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err.message });
+    console.error("Error in :", err);
+  }
+};
+
+module.exports = authController;
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../controller/user.controller');
+const userController = require("../controller/user.controller");
+const authController = require("../controller/auth.controller");
 
 // 1. 회원가입 endpoint
-router.post('/', userController.createUser);
+router.post("/", userController.createUser);
 
 // 2. 로그인 endpoint
-router.post('/login', userController.loginWithEmail);
+router.post("/login", userController.loginWithEmail);
+
+router.get("/me", authController.authenticate);
 
 module.exports = router;
